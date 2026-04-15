5a67c8a8f9e8a3c9de096e47cc38cb6185b60f7c
kzk <mykzk@yandex.ru>
2026-04-15 15:06:28 +0300

    API функции

--- cats/views.py
+++ cats/views.py
@@ -9,7 +9,7 @@ from .serializers import CatSerializer
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+        serializer = CatSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -17,3 +17,19 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'POST', 'PATCH', 'PUT'])
+def cat_detail(request, pk):
+    cat = Cat.objects.get(id=pk)
+    if request.method == 'PUT' or request.method == 'PATCH':
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'DELETE':
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    serializer = CatSerializer(cat)
+    return Response(serializer.data, status=status.HTTP_200_OK)
